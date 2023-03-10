545248f22a689f86f7c2cb4b4c84d6b2593c9772
0xadamm <adam.mourade@gmail.com>
2023-03-09 17:12:28 -0800

    added bio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,25 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div className=" relative w-full h-screen mx-auto">
+    <section className=" relative w-full h-screen mx-auto">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hi, I'm <span className={`${styles.heroNameText}`}>Adam</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            {" "}
+            I'm a Full Stack Blockchain Developer
+          </p>
+        </div>
       </div>
-    </div>
+      <ComputersCanvas />
+    </section>
   );
 };
 
