d40586cfc7f358bfd0eefcfe3fcf485287a2b6b7
0xadamm <adam.mourade@gmail.com>
2023-03-09 18:19:01 -0800

    created loader for obj

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html>
+      <span className="canvas-load"></span>
+      <p style={{ fontSize: 14, color: "", fontWeight: 800, marginTop: 40 }}>
+        {progress.toFixed(2)}%{" "}
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+export default Loader;
