3b516fa52e2505a10b5ce9e3dce02b6f6979a656
0xadamm <adam.mourade@gmail.com>
2023-03-09 18:15:18 -0800

    added spot light

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,6 +11,14 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
