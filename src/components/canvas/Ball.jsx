65680d0d6ef8102a93c03f1c60e72f2e38970c48
0xadamm <adam.mourade@gmail.com>
2023-03-11 18:17:10 -0800

    created ball canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -16,4 +16,20 @@ const Ball = props => {
   return <Float></Float>;
 };
 
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
 export default BallCanvas;
