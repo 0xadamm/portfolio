156df957eb707210a9ea851aa6e97356546fd546
0xadamm <adam.mourade@gmail.com>
2023-03-11 18:20:55 -0800

    created icosahedron inside of canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -13,7 +13,15 @@ import CanvasLoader from "../Loader";
 const Ball = props => {
   const [decal] = useTexture([props.imgUrl]);
 
-  return <Float></Float>;
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+      </mesh>
+    </Float>
+  );
 };
 
 const BallCanvas = ({ icon }) => {
