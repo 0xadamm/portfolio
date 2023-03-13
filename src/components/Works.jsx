b08775b36fe7145d30039ed6550286ca2d65897e
0xadamm <adam.mourade@gmail.com>
2023-03-13 11:58:56 -0700

    added bio

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,6 +16,17 @@ const Works = () => {
         <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Projects</h2>
       </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          The projects I have worked on are a true testament to my skills and
+          experience. These real-world examples showcase my ability to solve
+          complex problems, work with diverse technologies, and effectively
+          manage projects.
+        </motion.p>
+        <div className="mt-20 flex flex-wrap gap-7"></div>
+      </div>
     </>
   );
 };
