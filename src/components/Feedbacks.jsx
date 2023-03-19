664c87dd81ca805b2527b8aab5d45f0fd2275226
0xadamm <adam.mourade@gmail.com>
2023-03-18 19:58:03 -0700

    created testimonial container

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import SectionWrapper from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>
+            What other say
+          </p>
+          <h2 className={styles.sectionHeadText}>
+            Testimonials.
+          </h2>
+        </motion.div>
+      </div>
+      <div
+        className={`${styles.paddingX} mt-20 pb-14 flex flex-wrap gap-7`}></div>
+    </div>
+  );
+};
 
-export default Feedbacks
+export default Feedbacks;
